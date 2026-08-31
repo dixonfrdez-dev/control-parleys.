94a4afa9707c1d028b3a7a014e93b5487aed5fd2
dixonfrdez-dev <dixonfrdez@gmail.com>
2026-08-31 01:18:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def analizar_ticket_con_ia(imagen_pil):
         """
         
         response = client.models.generate_content(
-            model='gemini-2.5-flash',
+            model='gemini-2.0-flash',
             contents=[imagen_pil, prompt]
         )
         
